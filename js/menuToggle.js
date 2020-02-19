ca00a20aa8bf4c129c90bcf68a050052e1fcd557
Rachel Conatser <10806252@uvu.edu>
2020-02-19 14:18:17 -0700

    added images, css, page info, need to finish cards

---
+++ js/menuToggle.js
@@ -0,0 +1,10 @@
+//Create a button and give it an ID of "hamburgerBtn"
+//Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
+
+function toggleMenu() {
+   document.getElementById("hamburgerBtn").classList.toggle("open");
+   document.getElementById("mobile").classList.toggle("open");
+}
+var x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu; 
+
